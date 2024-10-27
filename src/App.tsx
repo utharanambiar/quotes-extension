cd85c9d339ca1b29ea66529a824b1e3c3ce0631c
utharanambiar <utharanambiar@gmail.com>
2024-10-27 19:34:42 +0530

    added random bg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Background from "./Components/Background";
 import Quotes from "./Components/Quotes";
 
 function App() {
   return (
     <div className="App">
       <Quotes />
+      <Background />
     </div>
   );
 }
---
+++ src/Components/Background.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import styles from "./background.module.scss";
+
+function Background() {
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_API_KEY}&orientation=landscape&collections=nature`
+    )
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        setData(data?.urls?.regular);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <img
+      src={data}
+      alt="altimage"
+      className={styles["background-image"]}
+    />
+  );
+}
+
+export default Background;
